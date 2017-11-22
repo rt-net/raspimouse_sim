1000c5cd8855cc9d04d6fa5d7d7f8d320c5d6289
yukixx6 <s1526707DA@s.chibakoudai.jp>
2017-11-22 20:03:51 +0900

    Add virtual_motors

---
+++ raspimouse_control/scripts/virtual_motors.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rospy, math
+from geometry_msgs.msg import Twist
+
+
+def motor_freq():
+    lfile = '/dev/rtmotor_raw_l0'
+    rfile = '/dev/rtmotor_raw_r0'
+    vel = Twist()
+
+    while not rospy.is_shutdown():
+        try:
+            with open(lfile,'r') as lf,\
+            open(rfile,'r') as rf:
+                lhz_str = lf.readline().rstrip()
+                rhz_str = rf.readline().rstrip()
+                if len(lhz_str)!=0 and len(rhz_str)!=0:
+
+
+                    lhz = int(lhz_str)
+                    rhz = int(rhz_str)
+                    vel.linear.x = (lhz+rhz)*9*math.pi/160000.0
+                    vel.angular.z = (rhz-lhz)*math.pi/800.0
+
+                    print vel
+                    pub.publish(vel)
+        except rospy.ROSInterruptException:
+            pass
+
+if __name__ == "__main__":
+    rospy.init_node('virtual_motors')
+    pub = rospy.Publisher('/raspimouse_on_gazebo/diff_drive_controller/cmd_vel', Twist, queue_size=10)
+    motor_freq()
+    rospy.spin()
+
+
