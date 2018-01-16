d369d347ad8efc665c649b4c33e78c53a74fb283
yukixx6 <s1526707DA@s.chibakoudai.jp>
2018-01-16 14:26:00 +0900

    Enable motorswitch

--- raspimouse_control/scripts/virtual_motors.py
+++ raspimouse_control/scripts/virtual_motors.py
@@ -4,26 +4,32 @@ from geometry_msgs.msg import Twist
 
 
 def motor_freq():
+    swfile ='/dev/rtmotoren0'
     lfile = '/dev/rtmotor_raw_l0'
     rfile = '/dev/rtmotor_raw_r0'
     vel = Twist()
+    sound_count = 0
 
     while not rospy.is_shutdown():
         try:
-            with open(lfile,'r') as lf,\
-            open(rfile,'r') as rf:
-                lhz_str = lf.readline().rstrip()
-                rhz_str = rf.readline().rstrip()
-                if len(lhz_str)!=0 and len(rhz_str)!=0:
-
-
-                    lhz = int(lhz_str)
-                    rhz = int(rhz_str)
-                    vel.linear.x = (lhz+rhz)*9*math.pi/160000.0
-                    vel.angular.z = (rhz-lhz)*math.pi/800.0
-
-                    print vel
-                    pub.publish(vel)
+            with open(swfile,'r') as f:
+                if f.readline().rstrip()=='1':
+                    if sound_count == 0:
+                        subprocess.call("aplay ~/catkin_ws/src/raspimouse_sim/raspimouse_control/scripts/ms_sound.wav", shell=True)
+                        sound_count = 1
+                    with open(lfile,'r') as lf,\
+                         open(rfile,'r') as rf:
+                        lhz_str = lf.readline().rstrip()
+                        rhz_str = rf.readline().rstrip()
+                        if len(lhz_str)!=0 and len(rhz_str)!=0:
+                            lhz = int(lhz_str)
+                            rhz = int(rhz_str)
+                            vel.linear.x = (lhz+rhz)*9*math.pi/160000.0
+                            vel.angular.z = (rhz-lhz)*math.pi/800.0
+                            print vel
+                            pub.publish(vel)
+                else:
+                    sound_count = 0
         except rospy.ROSInterruptException:
             pass
 
