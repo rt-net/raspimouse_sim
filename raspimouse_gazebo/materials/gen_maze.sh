7fc7c0c61dfd8fe5bc58280a9d5579ffce997f85
Daisuke Sato <daisuke.sato@rt-net.jp>
2020-10-21 20:04:36 +0900

    Use xacro instead of xacro.py

--- raspimouse_gazebo/materials/gen_maze.sh
+++ raspimouse_gazebo/materials/gen_maze.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -eu
 
-WORKINGDIR="$HOME/catkin_ws/src/raspimouse_sim/raspimouse_gazebo"
+WORKINGDIR="$(rospack find raspimouse_gazebo)"
 
-rosrun xacro xacro.py $WORKINGDIR/materials/sample_maze.world.xacro > $WORKINGDIR/worlds/sample_maze.world
+rosrun xacro xacro --inorder $WORKINGDIR/materials/sample_maze.world.xacro > $WORKINGDIR/worlds/sample_maze.world
