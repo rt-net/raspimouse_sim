2337686c3251653b4d1b72c2f3398aba60e51828
YusukeKato <yusuke.kato@rt-net.jp>
2023-09-06 18:52:47 +0900

    CIのテストが通るようにpackage.xmlを修正&ライセンス文を追加

--- raspimouse_gazebo/launch/raspimouse_with_emptyworld.launch.py
+++ raspimouse_gazebo/launch/raspimouse_with_emptyworld.launch.py
@@ -1,4 +1,23 @@
-# Copyright 2022 RT Corporation
+# The MIT License (MIT)
+#
+# Copyright 2023 RT Corporation <support@rt-net.jp>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+# the Software, and to permit persons to whom the Software is furnished to do so,
+# subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+# FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import os
 
@@ -9,6 +28,7 @@ from launch_ros.actions import SetParameter
 from launch_ros.actions import Node
 
 import xacro
+
 
 def generate_launch_description():
     env = {'IGN_GAZEBO_SYSTEM_PLUGIN_PATH': os.environ['LD_LIBRARY_PATH'],
